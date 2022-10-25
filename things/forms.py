aaac9dbd7944d399b706c2bcaa5a01c1522cf8ab
Areesha <areesha.durrani@hotmail.com>
2022-10-25 13:24:17 +0100

    Created new ThingForm and display and home page

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
-
+from django import forms
+from django.core.validators import RegexValidator
+from .models import Thing
 # Create your forms here.
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields=['name', 'description', 'quantity']
+        widgets = {'description' : forms.Textarea(), 'quantity':forms.NumberInput()}
+
+    def clean(self):
+        pass
